768794ca71ef7d714779c92838a425043c8cf959
Xfennec <xfennec@cqfd-corp.org>
2014-09-02 10:50:18 +0200

    Updating to 0.5 (less verbose)

--- cv.c
+++ cv.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2013 Xfennec, CQFD Corp.
+   Copyright (C) 2014 Xfennec, CQFD Corp.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- cv.h
+++ cv.h
@@ -8,7 +8,7 @@
 
 #include "hlist.h"
 
-#define CV_VERSION         "0.4"
+#define CV_VERSION         "0.5"
 
 #define PROC_PATH       "/proc"
 #define MAX_PIDS        32
