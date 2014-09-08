6e34651702fe7f0b77e9fc706351b6bfd18111b3
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2014-09-09 02:12:13 +0530

    Fixed Spaces.

--- Makefile
+++ Makefile
@@ -7,16 +7,16 @@ MANDIR = $(PREFIX)/share/man/man1
 
 $(OBJ) : cv.o sizes.o hlist.o
 	$(CC) -Wall $^ -o $@ $(LFLAGS)
-%.o : %.c
+	%.o : %.c
 	$(CC) $(CFLAGS) -c $^
 clean :
 	rm -f *.o $(OBJ)
-install : $(OBJ)
+	install : $(OBJ)
 	@echo "Installing program to $(DESTDIR)$(BINDIR) ..."
 	@mkdir -p $(DESTDIR)$(BINDIR)
 	@install -pm0755 $(OBJ) $(DESTDIR)$(BINDIR)/$(TARGET) || \
-	echo "Failed. Try "make PREFIX=~ install" ?"
+		echo "Failed. Try "make PREFIX=~ install" ?"
 	@echo "Installing manpage to $(DESTDIR)$(MANDIR) ..."
 	@mkdir -p $(DESTDIR)$(MANDIR)
 	@install -pm0644 cv.1 $(DESTDIR)$(MANDIR)/ || \
-	echo "Failed. Try "make PREFIX=~ install" ?"
+		echo "Failed. Try "make PREFIX=~ install" ?"
--- sizes.c
+++ sizes.c
@@ -6,25 +6,25 @@
 
 static const char     *sizes[]   = { "EiB", "PiB", "TiB", "GiB", "MiB", "KiB", "B" };
 static const uint64_t  exbibytes = 1024ULL * 1024ULL * 1024ULL *
-                                   1024ULL * 1024ULL * 1024ULL;
+1024ULL * 1024ULL * 1024ULL;
 
 void format_size(uint64_t size, char *result)
 {
-uint64_t multiplier;
-int i;
+	uint64_t multiplier;
+	int i;
 
-multiplier = exbibytes;
+	multiplier = exbibytes;
 
-for (i = 0 ; i < DIM(sizes) ; i++, multiplier /= 1024) {
-    if (size < multiplier)
-        continue;
-    if (size % multiplier == 0)
-        sprintf(result, "%" PRIu64 " %s", size / multiplier, sizes[i]);
-    else
-        sprintf(result, "%.1f %s", (float) size / multiplier, sizes[i]);
-    return;
-}
+	for (i = 0 ; i < DIM(sizes) ; i++, multiplier /= 1024) {
+		if (size < multiplier)
+			continue;
+		if (size % multiplier == 0)
+			sprintf(result, "%" PRIu64 " %s", size / multiplier, sizes[i]);
+		else
+			sprintf(result, "%.1f %s", (float) size / multiplier, sizes[i]);
+		return;
+	}
 
-strcpy(result, "0");
-return;
+	strcpy(result, "0");
+	return;
 }
