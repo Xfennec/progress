50298e8b0ef29c6b9d02a04bc1df1b6a879cd415
Xfennec <xfennec@cqfd-corp.org>
2016-01-14 16:04:12 +0100

    Updating copyrights to 2016

--- hlist.h
+++ hlist.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2015 Xfennec, CQFD Corp.
+   Copyright (C) 2016 Xfennec, CQFD Corp.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
